52e9f1da002a1f8f412a916be4c5aea4337946ed
kevinnguyen20 <kk.nguyen@web.de>
2022-11-06 09:37:22 +0100

    Add models created by DawidNiezgodka

---
+++ code/src/main/java/amos/specitemdatabase/model/SpecItem.java
@@ -0,0 +1,36 @@
+package amos.specitemdatabase.model;
+
+import java.util.List;
+import lombok.Getter;
+
+/**
+ * Represents a SpecItem together with the corresponding commit information.
+ */
+@Getter
+public class SpecItem {
+
+    private final String shortName;
+    private final Category category;
+    private final LcStatus lcStatus;
+    // private final Object useInstead; TODO: Ask the client about this field
+    // because it is always empty in the example data
+    private final List<String> traceRefs;
+    private final String longName;
+    private final String content;
+    private final Commit commit;
+
+    //TODO: Add the versioning mechanism
+    // private final short version;
+
+    // TODO: ask the client about the tags; where do they come from? shall they be included in the .txt file?
+    public SpecItem(final SpecItemBuilder specItemBuilder) {
+        this.shortName = specItemBuilder.getShortName();
+        this.category = specItemBuilder.getCategory();
+        this.lcStatus = specItemBuilder.getLcStatus();
+        this.traceRefs = specItemBuilder.getTraceRefs();
+        this.longName = specItemBuilder.getLongName();
+        this.content = specItemBuilder.getContent();
+        this.commit = specItemBuilder.getCommit();
+    }
+
+}
---
+++ code/src/main/java/amos/specitemdatabase/model/SpecItemEntity.java
@@ -0,0 +1,21 @@
+
+package amos.specitemdatabase.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SpecItemEntity {
+    private Long id;
+
+    // TODO: we might need
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
