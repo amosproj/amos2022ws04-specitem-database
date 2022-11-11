f9b238236eb07a14059f8426ad468c044f0a2d6d
gogokotsev00 <Kotsev75@gmail.com>
2022-11-11 22:02:40 +0100

    Removed unused imports

--- code/src/test/java/amos/specitemdatabase/model/SpecItemTest.java
+++ code/src/test/java/amos/specitemdatabase/model/SpecItemTest.java
@@ -5,10 +5,6 @@ import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 
 public class SpecItemTest {
 
