0477719cec7b216beff9614525b2356887b05592
v_ducthai <thaivd1309@gmail.com>
2022-11-21 20:28:32 +0100

    add Webconfig for CORS
    Signed-off-by: v_ducthai <thaivd1309@gmail.com>

---
+++ code/src/main/java/amos/specitemdatabase/config/WebConfig.java
@@ -0,0 +1,21 @@
+package amos.specitemdatabase.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan(useDefaultFilters = false, includeFilters = {
+        @ComponentScan.Filter(org.springframework.stereotype.Controller.class) })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
