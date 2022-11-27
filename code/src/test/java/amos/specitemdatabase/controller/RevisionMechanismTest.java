25d1e80b951acc8892857c5783a31df40acb4b39
Dawid Niezgódka <dawid.niezgodka@gmail.com>
2022-11-27 20:44:25 +0100

    finished test for the revision system
    Signed-off-by: Dawid Niezgódka <dawid.niezgodka@gmail.com>

--- code/src/test/java/amos/specitemdatabase/controller/RevisionMechanismTest.java
+++ code/src/test/java/amos/specitemdatabase/controller/RevisionMechanismTest.java
@@ -5,9 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import amos.specitemdatabase.model.SpecItem;
 import amos.specitemdatabase.repo.SpecItemRepo;
-import java.io.InputStream;
-import java.util.Objects;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,33 +46,119 @@ public class RevisionMechanismTest {
     @Test
     void shouldRetrieveTwoVersionsOfSpecItemAfterTwoInsertions() throws Exception {
 
-        final InputStream firstFileInputStream = this.getClass().getClassLoader()
-            .getResourceAsStream("testfiles/firstfile.txt");
-        final InputStream secondFileInputStream = this.getClass().getClassLoader()
-            .getResourceAsStream("testfiles/secondfile.txt");
+        final String firstFileContent = this.firstFileContent();
 
-        final MockMultipartFile firstFile = new MockMultipartFile(
-            "file1",
-            "firstfile.txt",
+        final MockMultipartFile firstFile
+            = new MockMultipartFile(
+            "file",
+            "test-file.txt",
             MediaType.TEXT_PLAIN_VALUE,
-            Objects.requireNonNull(firstFileInputStream).readAllBytes()
+            firstFileContent.getBytes()
         );
-        final MockMultipartFile secondFile = new MockMultipartFile(
-            "file2",
-            "secondfile.txt",
-            MediaType.TEXT_PLAIN_VALUE,
-            Objects.requireNonNull(secondFileInputStream).readAllBytes()
-        );
-
-        firstFileInputStream.close();
-        secondFileInputStream.close();
 
         mockMvc.perform(multipart("/upload/firstfile").file(firstFile))
             .andExpect(status().isCreated());
 
         assertEquals(2, specItemRepo.count());
 
+        final String secondFileContent = this.secondFileContent();
+
+        final MockMultipartFile secondFile
+            = new MockMultipartFile(
+            "file",
+            "test-file.txt",
+            MediaType.TEXT_PLAIN_VALUE,
+            secondFileContent.getBytes()
+        );
+
+        mockMvc.perform(multipart("/upload/firstfile").file(secondFile))
+            .andExpect(status().isCreated());
+
+        assertEquals(4, specItemRepo.count());
+
+        final List<SpecItem> specItems = specItemRepo.findAll();
+
+        Map<String, Long> specItemsToVersions = specItems.stream()
+            .map(SpecItem::getShortName)
+            .collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting()
+            ));
+        assertEquals(2, specItemsToVersions.get("SpecItem_0"));
 
     }
 
+
+    private String firstFileContent() {
+        return  "CommitHash: #abc\n" +
+            "CommitDate: 2022-02-12 21:00:00\n" +
+            "CommitMsg: bla bla prank\n" +
+            "CommitAuthor: Mister Wallace\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "Fingerprint: abc\n" +
+            "ShortName: SpecItem_0\n" +
+            "Category: Category1\n" +
+            "LC-Status: Status1\n" +
+            "UseInstead: \n" +
+            "TraceRefs: SpecItem_4\n" +
+            "LongName: LN1\n" +
+            "Content: \n" +
+            "content01\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "Fingerprint: def\n" +
+            "ShortName: SpecItem_1\n" +
+            "Category: Category2\n" +
+            "LC-Status: Status2\n" +
+            "UseInstead: \n" +
+            "TraceRefs: SpecItem_2\n" +
+            "LongName: LN2\n" +
+            "Content: \n" +
+            "content02:\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n";
+    }
+
+    private String secondFileContent() {
+        return  "CommitHash: #abcd\n" +
+            "CommitDate: 2022-02-12 21:10:00\n" +
+            "CommitMsg: bla bla prank\n" +
+            "CommitAuthor: Mister Wallace\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "Fingerprint: abc\n" +
+            "ShortName: SpecItem_0\n" +
+            "Category: Category1\n" +
+            "LC-Status: Status1\n" +
+            "UseInstead: \n" +
+            "TraceRefs: SpecItem_4\n" +
+            "LongName: LN1\n" +
+            "Content: \n" +
+            "content02\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "Fingerprint: defg\n" +
+            "ShortName: SpecItem_2\n" +
+            "Category: Category2\n" +
+            "LC-Status: Status2\n" +
+            "UseInstead: \n" +
+            "TraceRefs: SpecItem_2\n" +
+            "LongName: LN2\n" +
+            "Content: \n" +
+            "content12:\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n";
+    }
 }
