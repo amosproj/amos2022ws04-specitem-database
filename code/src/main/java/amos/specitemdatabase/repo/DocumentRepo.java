dd2183e013f33c7c075dfdb4f299decba9d41cc6
kevinnguyen20 <kk.nguyen@web.de>
2023-02-04 09:38:37 +0100

    Add annotation

--- code/src/main/java/amos/specitemdatabase/repo/DocumentRepo.java
+++ code/src/main/java/amos/specitemdatabase/repo/DocumentRepo.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepo extends JpaRepository<DocumentEntity, Long> {
     @Deprecated
     @Query(
